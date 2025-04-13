e6337dd770e04fd1e1a7ee340c58545c12cd557f
ChristianCage7 <christianandrade95@gmail.com>
2025-04-12 20:19:39 -0400

    Levantamiento angular
    Se levanta esqueleto app

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenu,
+  IonMenuButton,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonTitle, IonToolbar],})
+export class HomePage {
+  constructor() {}
+}
