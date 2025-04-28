bbc170f11aa28f31a9ec210101719af9a71a93bf
yarr.oz <yarod.aliaga@gmail.com>
2025-04-28 14:30:26 -0400

    Se corrigen los formularios, se arregla bug que botaba el sign in, se empieza con el service

--- src/app/pages/auth/sign-up/sign-up.page.ts
+++ src/app/pages/auth/sign-up/sign-up.page.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { SupabaseService } from 'src/app/shared/services/supabase.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -9,24 +11,35 @@ import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/fo
 
 })
 export class SignUpPage implements OnInit {
-  form = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required]),
-    confirmPassword: new FormControl('', [Validators.required]),    
-    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
-    firstName: new FormControl('', [Validators.required]),
-    lastName: new FormControl('', [Validators.required]),
-    nationalId: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    birthDate: new FormControl('', [Validators.required]),
-    gender: new FormControl('', [Validators.required])
-  }, { validators: this.passwordMatchValidator });
-  constructor() { }
+
+  form!: FormGroup;
+
+  constructor(
+    private supabaseService: SupabaseService,
+    private http : HttpClient,
+    private fb: FormBuilder
+  ) { 
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+      confirmPassword: new FormControl('', [Validators.required]),
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      nationalId: new FormControl('', [Validators.required]),
+      birthDate: new FormControl('', [Validators.required]),
+      gender: new FormControl('', [Validators.required])
+    }, {validators: this.passwordMatchValidator});
+  }
 
   ngOnInit() {
   }
 
   async submit(){
-    console.log(this.form.value);
+    if (this.form.invalid) return;
+  }
+
+  getFormControl(controlName:string): FormControl {
+    return this.form.get(controlName) as FormControl;
   }
 
   passwordMatchValidator(group: AbstractControl): { [key: string]: boolean } | null {
